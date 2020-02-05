19f7ce945d2309ec95d1814b02b5ebd87cbe5e71
manish263160 <manish263160@gmail.com>
2020-02-05 23:53:30 +0530

    demo 3 3rd commit

--- src/ScheduledExecutorsPeriodicExample.java
+++ src/ScheduledExecutorsPeriodicExample.java
@@ -6,7 +6,19 @@ import java.util.concurrent.TimeUnit;
  * Created by rajeevkumarsingh on 10/05/17.
  */
 public class ScheduledExecutorsPeriodicExample {
-    public static void main(String[] args) {
+  
+	  public static void main(String[] args) {
+	        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+	        Runnable task = () -> {
+	          System.out.println("Executing Task At " + System.nanoTime());
+	        };
+
+	        System.out.println("scheduling task to be executed every 2 seconds with an initial delay of 0 seconds");
+	        scheduledExecutorService.scheduleAtFixedRate(task, 0,2, TimeUnit.SECONDS);
+
+	    }
+	public static void main(String[] args) {
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
 
         Runnable task = () -> {
